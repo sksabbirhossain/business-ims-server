50bc36474ac4e85fee017c8cdde922296679aa58
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-12-10 19:59:15 +0600

    created add category validation

--- routes/admin/category/categoryRouter.js
+++ routes/admin/category/categoryRouter.js
@@ -3,10 +3,20 @@ const checkIsAdmin = require("../../../middleware/common/admin/checkIsAdmin");
 const {
   createCategory,
 } = require("../../../controller/admin/category/categoryController");
+const {
+  categoryValidators,
+  categoryValidationHandler,
+} = require("../../../validators/admin/category/categoryValidators");
 
 const router = express.Router();
 
 //create category
-router.post("/add-category", checkIsAdmin, createCategory);
+router.post(
+  "/add-category",
+  checkIsAdmin,
+  categoryValidators,
+  categoryValidationHandler,
+  createCategory
+);
 
 module.exports = router;
---
+++ validators/admin/category/categoryValidators.js
@@ -0,0 +1,44 @@
+const { validationResult, check } = require("express-validator");
+const createError = require("http-errors");
+
+const Category = require("../../../models/storeAdmin/categorySchema");
+
+const categoryValidators = [
+  check("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const user = await Category.findOne({ name: value });
+        if (user) {
+          throw createError("Category is already exists");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+  check("description")
+    .notEmpty()
+    .withMessage("Description is required")
+    .isLength({ min: 10 })
+    .withMessage("Description must-be 10 characters")
+    .trim(),
+];
+
+const categoryValidationHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    res.status(500).json({
+      errors: mappedErrors,
+    });
+  }
+};
+
+module.exports = {
+  categoryValidators,
+  categoryValidationHandler,
+};
