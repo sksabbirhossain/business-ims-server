e54b686a69cb8e450ce673ed6ac71096f1c56f79
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-03-26 03:56:36 +0600

    modified getCategories controller for pagination

--- controller/admin/category/categoryController.js
+++ controller/admin/category/categoryController.js
@@ -3,15 +3,29 @@ const Category = require("../../../models/storeAdmin/categorySchema");
 //get all category
 const getCategories = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+    const skip = (page - 1) * limit; // Calculate offset
+
+    // Get total count
+    const totalCategories = await Category.countDocuments({
+      storeInfo: req.store.storeId,
+    });
+
     //get category from database
-    const category = await Category.find({ storeInfo: req.store.storeId }).sort(
-      { createdAt: 1 }
-    );
+    const category = await Category.find({ storeInfo: req.store.storeId })
+      .sort({ createdAt: 1 })
+      .skip(skip)
+      .limit(limit);
 
     //send the response
     if (category && category.length >= 0) {
       res.json({
         data: category,
+        total: totalCategories,
+        currentPage: page,
+        totalPages: Math.ceil(totalCategories / limit),
+        limit: limit,
       });
     } else {
       res.json({
