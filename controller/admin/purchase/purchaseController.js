8ef1434e56aec86275e20638d141c28a84a34c5e
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-03-26 13:40:41 +0600

    created pagination query for getPurchases controller

--- controller/admin/purchase/purchaseController.js
+++ controller/admin/purchase/purchaseController.js
@@ -3,15 +3,30 @@ const Purchase = require("../../../models/storeAdmin/purchaseSchema");
 //get all purchase
 const getPurchases = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+    const skip = (page - 1) * limit; // Calculate offset
+
+    // Get total count
+    const totalPurchases = await Purchase.countDocuments({
+      storeInfo: req.store.storeId,
+    });
+
     //get category from database
     const purchase = await Purchase.find({ storeInfo: req.store.storeId })
       .sort({ createdAt: 1 })
+      .skip(skip)
+      .limit(limit)
       .populate("supplierInfo");
 
     //send the response
     if (purchase && purchase.length >= 0) {
       res.json({
         data: purchase,
+        total: totalPurchases,
+        currentPage: page,
+        totalPages: Math.ceil(totalPurchases / limit),
+        limit: limit,
       });
     } else {
       res.json({
