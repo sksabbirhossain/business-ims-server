dfcd32e46001df5e503cf14abef4fdce961ed2a7
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-03-29 17:21:30 +0600

    created customerSchema

---
+++ models/storeAdmin/customerSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const customerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    picture: {
+      type: String,
+    },
+    picture_info: {
+      public_key: String,
+    },
+    storeInfo: {
+      type: mongoose.Types.ObjectId,
+      ref: "Store",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
