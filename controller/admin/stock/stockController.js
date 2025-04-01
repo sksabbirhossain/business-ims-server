e1b1448e720b07dad77739bb68e3ea5dbd99d4f6
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-04-01 20:59:07 +0600

    modified getStocks controller for paginatin

--- controller/admin/stock/stockController.js
+++ controller/admin/stock/stockController.js
@@ -3,15 +3,30 @@ const Stock = require("../../../models/storeAdmin/stockSchema");
 //get all stock
 const getStocks = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+    const skip = (page - 1) * limit; // Calculate offset
+
+    // Get total count
+    const totalCustomers = await Stock.countDocuments({
+      storeInfo: req.store.storeId,
+    });
+
     //get category from database
     const stocks = await Stock.find({ storeInfo: req.store.storeId })
       .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
       .populate("supplierInfo");
 
     //send the response
     if (stocks && stocks.length >= 0) {
       res.json({
         data: stocks,
+        total: totalCustomers,
+        currentPage: page,
+        totalPages: Math.ceil(totalCustomers / limit),
+        limit: limit,
       });
     } else {
       res.json({
