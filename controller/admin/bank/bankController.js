dccf588f024769a1e5ab77bdcf4cb85f16358698
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-04-05 12:48:37 +0600

    created getBanks controller for get all banks

--- controller/admin/bank/bankController.js
+++ controller/admin/bank/bankController.js
@@ -1,5 +1,52 @@
 const Bank = require("../../../models/storeAdmin/bankSchema");
 
+//get all bank with pagination
+const getBanks = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+    const skip = (page - 1) * limit; // Calculate offset
+
+    // Get total count
+    const totalBanks = await Bank.countDocuments({
+      storeInfo: req.store.storeId,
+    });
+
+    //get bank from database
+    const bank = await Bank.find({ storeInfo: req.store.storeId })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    //send the response
+    if (bank && bank.length >= 0) {
+      res.json({
+        data: bank,
+        total: totalBanks,
+        currentPage: page,
+        totalPages: Math.ceil(totalBanks / limit),
+        limit: limit,
+      });
+    } else {
+      res.json({
+        errors: {
+          common: {
+            msg: "Unknown error occured!",
+          },
+        },
+      });
+    }
+  } catch (err) {
+    res.json({
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+};
+
 //create bank
 const createBank = async (req, res) => {
   try {
@@ -40,5 +87,6 @@ const createBank = async (req, res) => {
 };
 
 module.exports = {
+  getBanks,
   createBank,
 };
--- routes/admin/bank/bankRouter.js
+++ routes/admin/bank/bankRouter.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const checkIsAdmin = require("../../../middleware/common/admin/checkIsAdmin");
 const { createBank } = require("../../../controller/admin/bank/bankController");
+const {
+  bankValidators,
+  bankValidationHandler,
+} = require("../../../validators/admin/bank/bankValidators");
 
 const router = express.Router();
 
+//get all bank
+router.get("/bank-list", checkIsAdmin, getBanks);
+
 //create bank
-router.post("/create", checkIsAdmin, createBank)
+router.post(
+  "/create",
+  checkIsAdmin,
+  bankValidators,
+  bankValidationHandler,
+  createBank
+);
 
 module.exports = router;
