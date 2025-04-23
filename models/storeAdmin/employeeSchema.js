2eb226d80bc5ba632c5fab4211cb9a37295d65dc
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-04-23 22:42:51 +0600

    created employeeSchema

---
+++ models/storeAdmin/employeeSchema.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const SalaryHistorySchema = new mongoose.Schema({
+  month: {
+    type: String, // Example: 'April 2025'
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  paidAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const employeeSchema = new mongoose.Schema(
+  {
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Store",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    monthlySalary: {
+      type: Number,
+      required: true,
+    },
+    joiningDate: {
+      type: Date,
+      default: Date.now,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    salaryHistory: [SalaryHistorySchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
