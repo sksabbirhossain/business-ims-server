e6126a9770b03d94d26f7788b1073464ea30d335
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-04-24 13:22:57 +0600

    created getEmployees controller with pagination

--- controller/admin/employee/employeeController.js
+++ controller/admin/employee/employeeController.js
@@ -1,5 +1,52 @@
 const Employee = require("../../../models/storeAdmin/employeeSchema");
 
+//get employee with pagination
+const getEmployees = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+    const skip = (page - 1) * limit; // Calculate offset
+
+    // Get total count
+    const totalEmployee = await Employee.countDocuments({
+      storeInfo: req.store.storeId,
+    });
+
+    //get employee from database
+    const employee = await Employee.find({ storeInfo: req.store.storeId })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    //send the response
+    if (employee && employee.length >= 0) {
+      res.json({
+        data: employee,
+        total: totalEmployee,
+        currentPage: page,
+        totalPages: Math.ceil(totalEmployee / limit),
+        limit: limit,
+      });
+    } else {
+      res.json({
+        errors: {
+          common: {
+            msg: "Unknown error occured!",
+          },
+        },
+      });
+    }
+  } catch (err) {
+    res.json({
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+};
+
 //create employee
 const createEmployee = async (req, res) => {
   try {
@@ -40,6 +87,4 @@ const createEmployee = async (req, res) => {
   }
 };
 
-module.exports = {
-  createEmployee,
-};
+module.exports = { getEmployees, createEmployee };
--- routes/admin/employee/employeeRouter.js
+++ routes/admin/employee/employeeRouter.js
@@ -2,12 +2,29 @@ const express = require("express");
 const checkIsAdmin = require("../../../middleware/common/admin/checkIsAdmin");
 const {
   createEmployee,
+  getEmployees,
+  getEmployee,
 } = require("../../../controller/admin/employee/employeeController");
-const { employeeValidators, employeeValidationHandler } = require("../../../validators/admin/employee/employeeValidators");
+const {
+  employeeValidators,
+  employeeValidationHandler,
+} = require("../../../validators/admin/employee/employeeValidators");
 
 const router = express.Router();
 
+//get employee with pagination
+router.get("/employee-list", checkIsAdmin, getEmployees);
+
+// get a employee by employeeId
+router.get("/:employeeId", checkIsAdmin, getEmployee);
+
 //create employee
-router.post("/create", checkIsAdmin,employeeValidators,employeeValidationHandler, createEmployee);
+router.post(
+  "/create",
+  checkIsAdmin,
+  employeeValidators,
+  employeeValidationHandler,
+  createEmployee
+);
 
 module.exports = router;
